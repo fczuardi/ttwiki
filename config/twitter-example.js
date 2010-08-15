8863b5951a5adfaa2f3b77751b57bc67c777d78f
Fabricio Zuardi <fabricio@fcz.local>
2010-08-15 19:44:26 -0300

    config loading to setup twitter OAuth

---
+++ config/twitter-example.js
@@ -0,0 +1,8 @@
+exports.tokens = function(){
+  return {
+      consumer_key : 'YOUR_CONSUMER_KEY_HERE'
+    , consumer_secret : 'YOUR CONSUMER_SECRET_HERE'
+    , oauth_token : 'YOUR_ACCESS_TOKEN_HERE'
+    , oauth_token_secret : 'YOUR_ACCESS_TOKEN_SECRET_HERE'
+  }
+};
---
+++ twitter_oauth_test.js
@@ -0,0 +1,7 @@
+try{
+  var twitter_api_tokens = require('./config/twitter').tokens();
+}catch(e){
+  console.log('Error: You need to setup your Twitter OAuth tokens. Edit the file /config/twitter-example.js and save it as /config/twitter.js');
+  process.exit(1);
+}
+console.log(twitter_api_tokens);
